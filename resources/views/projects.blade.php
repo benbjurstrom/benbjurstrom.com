6995ccdf804641b76ce6951d50d3d17d590c1f87
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2024-08-12 09:46:22 -0700

    Fix project links

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -86,7 +86,7 @@
                                         <div
                                             class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:rounded-2xl"
                                         ></div>
-                                        <a href="http://planetaria.tech">
+                                        <a href="{{ $project['href'] }}">
                                             <span
                                                 class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"
                                             ></span>
