d0f6b84ac22491c84490aa4ddd92eef689d2af79
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2025-02-28 12:44:51 -0800

    Update canonical

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,10 @@
 @endphp
 
 <x-prezet::template>
+    @seo([
+        'url' => 'https://benbjurstrom.com'
+    ])
+
     <div class="mt-9 sm:px-8">
         <div class="mx-auto w-full max-w-7xl lg:px-8">
             <div class="relative px-4 sm:px-8 lg:px-12">
